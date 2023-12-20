40d06891a3f591b7554fbafa8134406990927c0e
Lambertn33 <lambertgithub@gmail.com>
2023-12-20 15:47:56 +0200

    feat: new wushlist form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,31 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Wishlist } from '../models/Wishlist';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'wishlists-app';
 
-  wishlists: Array<Wishlist> = [
-    new Wishlist(1, 'Learn Laravel Inertia', true),
-    new Wishlist(2, 'Learn Cypress', false),
-    new Wishlist(3, 'Learn Next.js', false),
-  ]
+  newWishlist = {
+    text: '',
+  };
+
+  wishlists: Array<Wishlist> = [];
+
+  toggleWishlist(wishlist: Wishlist) {
+    wishlist.isCompleted = !wishlist.isCompleted;
+    console.log(wishlist);
+  }
+
+  addNewWish() {
+    this.wishlists.push(new Wishlist(4, this.newWishlist.text, false));
+    this.newWishlist.text = '';
+  }
 }
