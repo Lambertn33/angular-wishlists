e82a85b9e5ce518d52ee4417e4948570867b236c
Lambertn33 <lambertgithub@gmail.com>
2023-12-20 15:08:23 +0200

    feat: list items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Wishlist } from '../models/Wishlist';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'wishlists-app';
+
+  wishlists: Array<Wishlist> = [
+    new Wishlist(1, 'Learn Laravel Inertia', true),
+    new Wishlist(2, 'Learn Cypress', false),
+    new Wishlist(3, 'Learn Next.js', false),
+  ]
 }
